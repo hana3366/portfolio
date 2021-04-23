4375849921f062ea5d6b69aba7bc22f9594dbb87
hana3366 <77098942+hana3366@users.noreply.github.com>
2021-04-23 11:48:25 +0900

    portfolio

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.menu').click(function(){
+        $('.menu').toggleClass("active")
+        
+        if($('.menu').hasClass("active")){
+            $('.menu2').show()
+            $('.menu img').attr('src','img/Layer 9 copy 2.png')
+        }else{
+            $('.menu2').hide()
+            $('.menu img').attr('src','img/Layer 14.png')
+        }
+    });
+
+});
